7d71d32f161d22b72ebee4a0a56692bd849ef554
AlaminPu1007 <alamin66.sit@gmail.com>
2024-08-09 12:17:39 +0600

    .husky shell updated

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -27,7 +27,7 @@ export const BFS_DELAY = 600;
 
 // create a dummy array of node value,
 // using this array we will create a binary tree
-export const TreeArrData: (number|null) [] = [
+export const TreeArrData: (number | null)[] = [
   50, 40, 60, 30, 35, 65, 70, 25, 78, 33, 37, 63, 68, 10, 80, 20, 22, 26, 29,
   31, 34, 36, 38, 61, 64, 66, 69, 72, 76, 78, 82,
 ];
