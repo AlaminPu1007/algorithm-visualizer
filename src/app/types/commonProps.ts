04757812759f2ac2a0e2c31bda2cac29499a0ecc
AlaminPu1007 <alamin66.sit@gmail.com>
2024-09-11 11:50:22 +0600

    add JSDoc comment for commonProps file

--- src/app/types/commonProps.ts
+++ src/app/types/commonProps.ts
@@ -1,9 +1,10 @@
 /**
- * Status colors component interface data Props.
+ * Represents a color configuration for status indicators.
  *
- * @export
  * @interface StatusColorsDataProps
- * @typedef {StatusColorsDataProps}
+ * @property {number} id - A unique identifier for the status color entry.
+ * @property {string} title - The descriptive title of the status.
+ * @property {string} bg_color - The background color associated with the status, specified as a color string (e.g., hex, rgb, or color name).
  */
 export interface StatusColorsDataProps {
   id: number;
